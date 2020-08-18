6aabde72c5b90ec8f7b422e1e6c3aa8242426585
ryan <csxrzeng@gmail.com>
2020-08-18 11:11:48 +0800

    Fix Parallel error

--- src/composites/index.js
+++ src/composites/index.js
@@ -1,4 +1,6 @@
-export {default as MemPriority} from './MemPriority';
-export {default as MemSequence} from './MemSequence';
-export {default as Priority} from './Priority';
-export {default as Sequence} from './Sequence';
+export { default as MemPriority } from './MemPriority';
+export { default as MemSequence } from './MemSequence';
+export { default as Priority } from './Priority';
+export { default as Sequence } from './Sequence';
+export { default as Parallel } from './Parallel';
+export { default as MemParallel } from './MemParallel';
